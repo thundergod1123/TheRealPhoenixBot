bf70327703ec6162eba9a20d8f7a0bbea0276bb4
VOIDWALKER <70213122+thundergod1123@users.noreply.github.com>
2021-01-28 01:58:26 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -19,18 +19,17 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-Hi {}, my name is {}!
-
-You can find the list of available commands with /help.
+Hi {}, my name is NERO
+use /help to see available commands.
 """
 
-BOT_IMAGE = "https://telegra.ph/file/93612a540608640355f20.mp4"
+BOT_IMAGE = "https://telegra.ph/file/3654994f3fdf85c0e8f15.jpg"
 
 HELP_STRINGS = """
-Hey there! My name is *{}*.
-I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of \
+heya! My name is NERO aka Secre Swallowtail.
+I'm a  with a few fun extras! Have a look at the following for an idea of some of \
 the things I can help you with.
-If you have any questions on how to use me, head over to @PhoenixSupport
+
 
 *Main* commands available:
  - /start: start the bot
